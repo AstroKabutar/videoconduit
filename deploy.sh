b4e2770e37656badef7e7afde527304cb60264cf
Shahin <shaodw@realm.go>
2026-03-07 09:32:26 +0530

    skeleton code

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# with full path example 'css/index.css'
+files-to-be-deployed=(
+
+)
+
+for file in ${files-to-be-deployed[@]}; do
+    echo "Deploying $file"
+    aws s3 cp $file $bucket_name/$file
+done
